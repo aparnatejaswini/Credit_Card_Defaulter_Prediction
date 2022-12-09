a50be1290202713ffb7b7bedf802786873d05a1f
Aparna <aparnaparkala@gmail.com>
2022-12-09 20:02:58 +0000

    entity artifact modified

--- defaulter_prediction/entity/entity_artifact.py
+++ defaulter_prediction/entity/entity_artifact.py
@@ -1,11 +1,48 @@
-from collections import namedtuple
+from dataclasses import dataclass
 
-DataIngestionArtifact =  namedtuple("DataIngestionArtifact",  
-                                    ["train_file_path", "test_file_path", "is_ingested", "msg"])
+@dataclass
+class DataIngestionArtifact:
+    train_file_path: str
+    test_file_path: str
 
-DataValidationArtifact = namedtuple("DataValidationArtifact", 
-                                    ["schema_file_path", "good_data_path", "report_file_path", "report_page_file_path", "is_validated", "msg"])
 
-DataTransformationArtifact = namedtuple("DataTransformationArtifact",
-                                        ["transformed_train_file_path", "transformed_test_file_path", "preprocessed_object_file_path",
-                                            "is_transformed", "msg"])
+@dataclass
+class DataValidationArtifact:
+    valid_train_file_path: bool
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
+    validation_status: bool
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+    preprocessed_object_file_path: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+
+@dataclass
+class ModelTrainerArtifact:    
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
+
+@dataclass
+class ModelEvaluationArtifact:
+    best_model_path: str
+    trained_model_path: str
+    improved_accuracy: float
+    is_model_accepted: bool
+    train_model_metric_artifact: ClassificationMetricArtifact
+    best_model_metric_artifact: ClassificationMetricArtifact
+
+@dataclass
+class ModelDeployArtifact:
+    model_file_path: str
+    saved_model_path: str
